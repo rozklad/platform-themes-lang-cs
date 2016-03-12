25ef748d01218f75d6aae51b27cffca94e6c2fde
rozklad <jan.rozklad@gmail.com>
2016-03-12 19:28:57 +0100

    Default language

---
+++ settings.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Part of the Platform Themes extension.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the WTFPL license.
+ *
+ * @package    Platform Themes extension
+ * @version    3.0.0
+ * @author     Sanatorium
+ * @license    WTFPL
+ * @copyright  2016, Sanatorium
+ * @link       http://sanatorium.ninja
+ */
+
+return [
+
+    // Section title
+    'title' => 'Šablony',
+
+    // Group
+    'general' => [
+
+        // Group title
+        'title' => 'Obecné',
+
+        // Fields
+        'field' => [
+            'area' => ':area šablona',
+        ],
+
+    ],
+
+    // Group
+    'settings' => [
+
+        // Group title
+        'title' => 'Nastavení',
+
+        // Fields
+        'field' => [
+            'debug'           => 'Režim ladění',
+            'force_recompile' => 'Vynutit kompilaci šablon',
+            'auto_clear'      => 'Automaticky mazat kompilované šablony',
+        ],
+
+    ],
+
+];
